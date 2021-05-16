e7864587f1712c8c716b91204d065e3340860540
Cesar Poumian <cepoumian@Cesars-MacBook-Pro.local>
2021-05-16 16:17:32 -0600

    * Create Teams and Team component. The Teams component follows the nested Route pattern using the Team component to fetch the selected team's data and render its UI accordingly. * Create Articles and Article component. The Articles component follows the nested Route pattern using the Article component to fetch the selected articles's data and render its UI accordingly. In Articles, Sidebar displays the available articles for the selected team. * Create Loading component.

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,8 @@ import Home from "./Home";
 import Players from "./Players";
 import Teams from "./Teams";
 import Navbar from "./Navbar";
+import TeamPage from "./TeamPage";
+import Articles from "./Articles";
 
 class App extends Component {
   render() {
@@ -15,6 +17,8 @@ class App extends Component {
             <Route path="/" exact component={Home} />
             <Route path="/players" component={Players} />
             <Route path="/teams" component={Teams} />
+            <Route path="/:teamId" exact component={TeamPage} />
+            <Route path="/:teamId/articles" component={Articles} />
             <Route render={() => <h1 className="text-center">Four oh Four</h1>} />
           </Switch>
         </div>
